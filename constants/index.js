15d1d0bf0528214257d89ad47dbd12dfe60fb834
Hassanz1984 <81552336+Hassanz1984@users.noreply.github.com>
2023-07-31 16:32:53 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x78f2b95e02972b6ed1f046e4e9b3e91206a0477f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
